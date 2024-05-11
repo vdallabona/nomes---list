c39f8287eca909f098633fce539bb691ffa269f2
vdallabona <vinicius.dallabona@gmail.com>
2024-05-11 10:11:10 -0300

    Nomes - List

---
+++ model/EditarLista.cs
@@ -0,0 +1,87 @@
+namespace Batata
+{
+    public class EditarLista
+    {
+        static List<string> itens = [];
+        static List<int> indexDeletados = [];
+        public static void CriarNome()
+        {
+            Console.WriteLine("Digite um nome: ");
+            string nome = Console.ReadLine() ?? "";
+            itens.Add(nome);
+            Console.WriteLine($"O nome {nome}, foi adicionado com sucesso.");
+        }
+        public static void LerNomes()
+        {
+            if (itens.Count > 0)
+            {
+                Console.WriteLine("------------- Os nomes cadastrados são -------------");
+                foreach (string item in itens)
+                {
+                    Console.Write(item);
+                    Console.WriteLine($" índice - {itens.IndexOf(item)}");
+                }
+                Console.WriteLine("----------------------------------------------------");
+                //  itens.ForEach(Console.WriteLine); Também dava pra fazer desse jeito que é menor
+            }
+            else
+            {
+                Console.WriteLine("Lista de nome está vazia.");
+            }
+        }
+        public static void AlterarNomes()
+        {
+            Console.WriteLine("Informe o índice do nome para alterar: ");
+            int indice = Convert.ToInt32(Console.ReadLine());
+            if (indice >= 0 && indice < itens.Count)
+            {
+                Console.WriteLine("Digite o novo nome: ");
+                string novoNome = Console.ReadLine() ?? "";
+                string nomeAntigo = itens[indice];
+                itens[indice] = novoNome;
+                Console.WriteLine($"Nome alterado de {nomeAntigo} para {itens[indice]}.");
+            }
+            else
+            {
+                Console.WriteLine("Indice inválido.");
+            }
+        }
+        public static void DeletarNomes()
+        {
+            int deletado;
+            int op;
+            Console.WriteLine("------------- Os nomes cadastrados são -------------");
+            foreach (string item in itens)
+            {
+                Console.Write(item);
+                Console.WriteLine($" índice - {itens.IndexOf(item)}");
+            }
+            Console.WriteLine("----------------------------------------------------");
+            do{
+                Console.WriteLine("Informe o índice do nome para deletar: ");
+                int indice = Convert.ToInt32(Console.ReadLine());
+                if (indice >= 0 && indice < itens.Count)
+                {
+                    string nomeDeletado = itens[indice];
+                    indexDeletados.Add(indice);
+                    Console.WriteLine($"Nome {nomeDeletado} será deletado.");
+                }
+                else
+                {
+                    Console.WriteLine("Indice inválido.");
+                }
+                Console.WriteLine("Deseja deletar outro nome?");
+                Console.WriteLine("[1] - Sim");
+                Console.WriteLine("[2] - Não");
+                op = Convert.ToInt32(Console.ReadLine());
+            }while(op == 1);
+            for (int i = 0; i < indexDeletados.Count; i++)
+            {
+                deletado = indexDeletados[i];
+                Console.WriteLine($"Deletando o nome ");
+                Console.WriteLine(itens[deletado]);
+                itens.RemoveAt(deletado);
+            }
+        }
+    }
+}
---
+++ program.cs
@@ -0,0 +1,47 @@
+﻿namespace Batata
+{
+    public class Pessoa
+    {
+
+        public static void Main()
+        {
+            int op;
+            do
+            {
+                Console.WriteLine("1 - Criar nome");
+                Console.WriteLine("2 - Ler nome");
+                Console.WriteLine("3 - Atualizar nome");
+                Console.WriteLine("4 - Deletar nome");
+                Console.WriteLine("5 - Sair");
+                op = Convert.ToInt32(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 1:
+                        EditarLista.CriarNome();
+                        break;
+
+                    case 2:
+                        EditarLista.LerNomes();
+                        break;
+
+                    case 3:
+                        EditarLista.AlterarNomes();
+                        break;
+
+                    case 4:
+                        EditarLista.DeletarNomes();
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Saindo.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        break;
+                }
+            } while (op != 5);
+        }
+    }
+}
